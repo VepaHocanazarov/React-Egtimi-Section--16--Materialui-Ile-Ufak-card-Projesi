1b1b2dda2465e2d2d0673c84d2101c71d1f31806
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-07 08:59:28 +0300

    AppBar Oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 function App() {
   return (
     <div >
-    
+     <AppBar>
+            <Toolbar sx={{fontSize:30}} >Kart Projesi</Toolbar>
+        </AppBar>
     </div>
   );
 }
